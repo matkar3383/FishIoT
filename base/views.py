2e4d9809afb3ca3a0418555d5f187ea9af8e3f2d
matkar3383 <mateusz.karasiewicz1990@gmail.com>
2023-11-27 23:15:23 +0100

    Initial commit with first site conf

--- base/views.py
+++ base/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+#function calling the page
+rooms = [
+    {'id': 1, 'name': 'zagrajmy'},
+    {'id': 2, 'name': 'przegrajmy'},
+    {'id': 3, 'name': 'chuj wie -- test'},
+]
+
+def home(request):
+    context = {'rooms':rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room', room}
+    return render(request, 'base/room.html', context)
+
+##TODO tu jest błąd gdzieś!!!
